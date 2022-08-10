49331dd7b6e7a0670ad10c1688f3312deee3c772
Rushabh Shah <rushabh795@gmail.com>
2022-08-09 21:58:05 -0400

    New Changes
    - Added SharedPref Manager class
    - Roll dice once functionality added
    -Added GSON
    - Saved data into SharedPref through GSON Done
    -Read Data from SharedPref through GSON Done.

--- app/src/main/java/com/rushabh/diceapp/MainActivity.java
+++ app/src/main/java/com/rushabh/diceapp/MainActivity.java
@@ -6,17 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 Spinner spChooseSide;
     Integer[] arrChooseDiceSide = {4,6,8,10,12,20};
 TextInputEditText edCustomDiceSide;
-MaterialButton btAdd;
+MaterialButton btAdd,btRollOnce,btRollTwice;
+TextView tvRollOneText,tvRollTwoText;
+ArrayList<String> arrListViewArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,13 @@ MaterialButton btAdd;
         spChooseSide = findViewById(R.id.spChooseSide);
         edCustomDiceSide=findViewById(R.id.edCustomDiceSide);
         btAdd = findViewById(R.id.btAdd);
+        btRollOnce = findViewById(R.id.btRollOnce);
+        btRollTwice = findViewById(R.id.btRollTwice);
+        tvRollOneText = findViewById(R.id.tvRollOneText);
+        tvRollTwoText = findViewById(R.id.tvRollTwoText);
+        SharedPrefManager.init(MainActivity.this);
         setSpinnerData(arrChooseDiceSide);
+        arrListViewArray = new ArrayList<>();
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,11 +65,52 @@ MaterialButton btAdd;
                 }
             }
         });
+        btRollOnce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stID = spChooseSide.getSelectedItem().toString();
+                Dice d = new Dice(Integer.parseInt(stID));
+                tvRollTwoText.setVisibility(View.GONE);
+                diceResultBeforeAndAfter(d);
+            }
+        });
     }
     private void setSpinnerData(Integer[] arrChooseSide) {
         ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(this,   android.R.layout.simple_dropdown_item_1line, arrChooseSide);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line); // The drop down view
         spChooseSide.setAdapter(spinnerArrayAdapter);
     }
+    private void diceResultBeforeAndAfter(Dice dice) {
+        dice.roll();
+        int sideUpAfterRoll = dice.getSideUp();
+        tvRollOneText.setText(String.valueOf(sideUpAfterRoll).toString());
+        saveDataToSharePref();
+    }
 
+    private void saveDataToSharePref() {
+        String nameData = tvRollOneText.getText().toString();
+        arrListViewArray.add(nameData);
+        //Using Gson i am saving data
+        Gson gson = new Gson();
+        //Saving string to Gson
+        String json = gson.toJson(arrListViewArray);
+        //pass object into sharePref
+SharedPrefManager.putString("newSaveObject",json);
+getDataFromSharedPref();
+    }
+
+    private void getDataFromSharedPref() {
+        Gson gson = new Gson();
+        String json = SharedPrefManager.getString("newSaveObject", "");
+        if (json.isEmpty()) {
+            Toast.makeText(MainActivity.this, "There is something error", Toast.LENGTH_LONG).show();
+        } else {
+            Type type = new TypeToken<List<String>>() {
+            }.getType();
+            List<String> arrPackageData = gson.fromJson(json, type);
+            for (String data : arrPackageData) {
+
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/rushabh/diceapp/SharedPrefManager.java
@@ -0,0 +1,51 @@
+package com.rushabh.diceapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+//Manage Shared Pref from this class
+public class SharedPrefManager {
+    private static boolean isInit = false;
+    private static SharedPreferences prefs;
+    private static SharedPreferences.Editor editor;
+    public final static String PREF_NAME = "Data_Save";
+
+    /*
+     * Constant Strings: username, password, cameraip, port, ptzip, ptzport,
+     * ptzusername, ptzpassword values-> Srings: temperture,
+     * voltageac,voltagedc,bligecount,. boolean:
+     * bilgepump,bilgelevel,bilgeintrusion,smoke
+     */
+
+    /**
+     * init AndroidSharedPreferences ,must be called before use
+     * AndroidSharedPreferences
+     *
+     * @param context
+     */
+
+    public static void init(Context context) {
+        if (isInit)
+            return;
+        prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = prefs.edit();
+        isInit = true;
+    }
+    public static void putString(String key, String value) {
+        editor.putString(key, value);
+        editor.commit();
+    }
+    public static void putInt(String key, int value) {
+        editor.putInt(key, value);
+        editor.commit();
+    }
+    public static String getString(String key, String defValue) {
+        return prefs.getString(key, defValue);
+    }
+    public static int getInt(String key, int defValue) {
+        return prefs.getInt(key, defValue);
+    }
+    public static void RemoveAllData() {
+        editor.clear().commit();
+    }
+}
